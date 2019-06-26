950df8b0c59348515aaf7d06a5cdbf7897dad0dd
Zach Bray <zachbray@googlemail.com>
2019-06-26 17:25:18 +0100

    Initial commit of basic publisher and subscriber working over docker.

---
+++ subscriber/run_subscriber.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+subscriber_channel="aeron:udp?endpoint=$SUBSCRIBER_IP_ADDRESS:$SUBSCRIBER_PORT"
+
+echo "SUBSCRIBER_CHANNEL=$subscriber_channel"
+echo "SUBSCRIBER_STREAM_ID=$SUBSCRIBER_STREAM_ID"
+
+java -cp /lib/aeron-all.jar \
+     -Daeron.sample.embeddedMediaDriver=true \
+     -Daeron.sample.channel=$subscriber_channel \
+     -Daeron.sample.streamId=$SUBSCRIBER_STREAM_ID \
+     io.aeron.samples.BasicSubscriber
+
