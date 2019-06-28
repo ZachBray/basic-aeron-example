39829745cdd44d62f8c2258ab337c774cf19bf83
Zach Bray <zachbray@googlemail.com>
2019-06-28 16:26:30 +0000

    Factor out common containers.

--- containers/publisher/run_publisher.sh
+++ containers/publisher/run_publisher.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-subscriber_channel="aeron:udp?endpoint=$SUBSCRIBER_IP_ADDRESS:$SUBSCRIBER_PORT"
-
-echo "SUBSCRIBER_CHANNEL=$subscriber_channel"
+echo "SUBSCRIBER_CHANNEL=$SUBSCRIBER_CHANNEL"
 echo "SUBSCRIBER_STREAM_ID=$SUBSCRIBER_STREAM_ID"
 
 java -cp /lib/aeron-all.jar \
      -Daeron.sample.embeddedMediaDriver=true \
-     -Daeron.sample.channel=$subscriber_channel \
+     -Daeron.sample.channel=$SUBSCRIBER_CHANNEL \
      -Daeron.sample.streamId=$SUBSCRIBER_STREAM_ID \
      io.aeron.samples.BasicPublisher
 
